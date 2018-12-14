a5114bd26d576e10db5c9476246e61d0bbe99630
dangchithao <dangchithao@gmail.com>
2018-12-14 16:35:04 +0700

    first commit

---
+++ views/site/image.php
@@ -0,0 +1,123 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\helpers\Html;
+
+$this->title = 'Images';
+$this->params['breadcrumbs'][] = $this->title;
+$this->registerCssFile('@web/css/image.css');
+?>
+<div class="site-about">
+    <h1><?= Html::encode($this->title) ?></h1>
+</div>
+
+<!--<div class="row">-->
+<!--    <div class="col-md-3">-->
+<!--        <div class="chanel-image center-block">-->
+<!--            <img src="./img/fruit01.png" alt="..." class="img-responsive center-block">-->
+<!--        </div>-->
+<!--    </div>-->
+<!--    <div class="col-md-3">-->
+<!--        <div class="chanel-image center-block">-->
+<!--            <img src="./img/fruit02.png" alt="..." class="img-responsive center-block">-->
+<!--        </div>-->
+<!--    </div>-->
+<!--    <div class="col-md-3">-->
+<!--        <div class="chanel-image center-block">-->
+<!--            <img src="./img/fruit03.png" alt="..." class="img-responsive center-block">-->
+<!--        </div>-->
+<!--    </div>-->
+<!--    <div class="col-md-3">-->
+<!--        <div class="chanel-image center-block">-->
+<!--            <img src="./img/fruit04.png" alt="..." class="img-responsive center-block">-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</div>-->
+
+<div class="row" style="margin-top: 30px;">
+    <div class="col-md-3">
+        <div class="chanel-image center-block">
+            <img src="./img/gachoi01.jpg" alt="..." class="img-responsive center-block">
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="chanel-image center-block">
+            <img src="./img/gachoi02.jpg" alt="..." class="img-responsive center-block">
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="chanel-image center-block">
+            <img src="./img/gachoi03.jpg" alt="..." class="img-responsive center-block">
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="chanel-image center-block">
+            <img src="./img/gachoi04.jpg" alt="..." class="img-responsive center-block">
+        </div>
+    </div>
+</div>
+
+
+<div class="row" style="margin-top: 30px;">
+    <div class="col-md-3">
+        <div class="chanel-image center-block">
+            <img src="./img/gachoi05.jpg" alt="..." class="img-responsive center-block">
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="chanel-image center-block">
+            <img src="./img/gachoi06.jpg" alt="..." class="img-responsive center-block">
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="chanel-image center-block">
+            <img src="./img/gachoi07.jpg" alt="..." class="img-responsive center-block">
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="chanel-image center-block">
+            <img src="./img/gachoi08.jpg" alt="..." class="img-responsive center-block">
+        </div>
+    </div>
+</div>
+
+<div class="row" style="margin-top: 30px;">
+    <div class="col-md-3">
+        <div class="chanel-image center-block">
+            <img src="./img/gachoi09.jpg" alt="..." class="img-responsive center-block">
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="chanel-image center-block">
+            <img src="./img/gachoi10.jpg" alt="..." class="img-responsive center-block">
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="chanel-image center-block">
+            <img src="./img/gachoi11.jpg" alt="..." class="img-responsive center-block">
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="chanel-image center-block">
+            <img src="./img/gachoi12.jpg" alt="..." class="img-responsive center-block">
+        </div>
+    </div>
+</div>
+
+<!--<div class="row">-->
+<!--    <div class="column">-->
+<!--            <img src="./img/fruit01.png" alt="..." class="img-responsive center-block">-->
+<!--            <img src="./img/fruit02.png" alt="..." class="img-responsive center-block">-->
+<!--            <img src="./img/fruit03.png" alt="..." class="img-responsive center-block">-->
+<!--            <img src="./img/fruit04.png" alt="..." class="img-responsive center-block">-->
+<!--    </div>-->
+<!---->
+<!--    <div class="column">-->
+<!--            <img src="./img/gachoi01.jpg" alt="..." class="img-responsive center-block">-->
+<!--            <img src="./img/gachoi02.jpg" alt="..." class="img-responsive center-block">-->
+<!--            <img src="./img/gachoi03.jpg" alt="..." class="img-responsive center-block">-->
+<!--            <img src="./img/gachoi04.jpg" alt="..." class="img-responsive center-block">-->
+<!--    </div>-->
+<!--</div>-->
+
